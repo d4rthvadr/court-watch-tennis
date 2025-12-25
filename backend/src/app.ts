e69a6da1970d412cc26c982356718a1e4e53b6f9
darkvadr <glowburn.acc@gmail.com>
2025-12-25 14:47:35 +0000

    feat: refactor player data handling and update client connection management

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,9 +4,9 @@ import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 4001;
-const updateInterval = 5 * 1000; // 15 seconds
+const updateDefaultInterval = 5 * 1000; // 15 seconds
 
-const clientStore = new Map<string, Response>();
+const connectedClients = new Map<string, Response>();
 
 app.use(cors());
 
@@ -25,6 +25,82 @@ const getUpdateInterval = (
   const parsed = parseInt(interval || "");
   return isNaN(parsed) ? defaultInterval : parsed;
 };
+interface Player {
+  name: string;
+  status: string;
+  rank: string;
+}
+interface PlayerWithGameStatus extends Player {
+  gameStatus: string;
+  court: string;
+}
+
+const initialPlayers: PlayerWithGameStatus[] = [
+  {
+    name: "Rodger Federer",
+    status: "Active",
+    rank: "1",
+    gameStatus: "Playing",
+    court: "Court 1",
+  },
+  {
+    name: "Rafael Nadal",
+    status: "Pending",
+    rank: "2  ",
+    gameStatus: "Waiting",
+    court: "Court 2",
+  },
+  {
+    name: "Novak Djokovic",
+    status: "Unpaid",
+    rank: "3",
+    gameStatus: "Waiting",
+    court: "Court 3",
+  },
+  {
+    name: "Andy Murray",
+    status: "Paid",
+    rank: "4",
+    gameStatus: "Playing",
+    court: "Court 4",
+  },
+  {
+    name: "Stan Wawrinka",
+    status: "Paid",
+    rank: "5",
+    gameStatus: "Playing",
+    court: "Court 5",
+  },
+  {
+    name: "Nick Kyrgios",
+    status: "Pending",
+    rank: "6",
+    gameStatus: "Waiting",
+    court: "Court 6",
+  },
+  {
+    name: "Juan Martin del Potro",
+    status: "Unpaid",
+    rank: "7",
+    gameStatus: "Waiting",
+    court: "Court 7",
+  },
+];
+
+const randomizePlayerData = (players: typeof initialPlayers) => {
+  return players.map((player) => {
+    const gameStatuses = ["Paid", "Pending", "Unpaid"];
+    const playerStatuses = ["Playing", "Waiting"];
+    const randomGameStatus =
+      gameStatuses[Math.floor(Math.random() * gameStatuses.length)];
+    return {
+      ...player,
+      rank: (Math.floor(Math.random() * 100) + 1).toString(),
+      status: playerStatuses[Math.floor(Math.random() * playerStatuses.length)],
+      gameStatus: randomGameStatus,
+    };
+  });
+};
 app.get("/sse", (req: Request, res: Response) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
@@ -32,7 +108,7 @@ app.get("/sse", (req: Request, res: Response) => {
 
   const clientId = getClientId(req.query?.clientId?.toString());
   const clientUpdateInterval = getUpdateInterval(
-    updateInterval,
+    updateDefaultInterval,
     req.query?.updateInterval?.toString()
   );
 
@@ -40,23 +116,26 @@ app.get("/sse", (req: Request, res: Response) => {
     `Client ${clientId} connected with update interval ${clientUpdateInterval}ms`
   );
 
-  clientStore.set(clientId, res);
+  connectedClients.set(clientId, res);
 
   console.log("Client connected to /sse");
 
-  const sendEvent = (data: string) => {
-    res.write(`data: ${data}\n\n`);
+  const sendEvent = () => {
+    const randomizedPlayers = randomizePlayerData(initialPlayers);
+    const eventData = JSON.stringify(randomizedPlayers);
+    res.write(`data: ${eventData}\n\n`);
   };
 
   const intervalId = setInterval(() => {
-    const eventData = `Current time is: ${new Date().toISOString()}`;
-    sendEvent(eventData);
+    sendEvent();
   }, clientUpdateInterval);
 
   req.on("close", () => {
-    const isRemoved = clientStore.delete(clientId);
+    const isRemoved = connectedClients.delete(clientId);
     console.log(
       `Client ${clientId} disconnected from /sse`,
+      " Remaining clients:",
+      connectedClients.size,
       isRemoved ? "" : "(was not found)"
     );
     clearInterval(intervalId);
