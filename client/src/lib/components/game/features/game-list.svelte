c75206374873de598b3c99eca5f6cb62615d6fc8
darkvadr <glowburn.acc@gmail.com>
2026-01-03 04:11:03 +0000

    feat: implement game addition and listing components with dialog support

---
+++ client/src/lib/components/game/features/game-list.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { gamesStore, type Game } from "$lib/stores/games";
+	import GameTable from "$lib/components/shared/game-table.svelte";
+
+	let games = $state<Game[]>([]);
+
+	// Subscribe to store
+	gamesStore.subscribe((value) => {
+		games = value;
+	});
+</script>
+
+<GameTable {games} />
---
+++ client/src/lib/components/game/features/index.ts
@@ -0,0 +1,2 @@
+export { default as GameAdd } from './game-add.svelte';
+export { default as GameList } from './game-list.svelte';
