417d7bcbc17144a70a8302d97cd9d9da69a94389
darkvadr <glowburn.acc@gmail.com>
2026-02-10 09:14:32 +0000

    feat: enhance graceful shutdown process with database disconnection and improved client notification

--- backend/src/util.ts
+++ backend/src/util.ts
@@ -1,43 +1,39 @@
 import { NextFunction } from "express";
 import { connectedClients } from "./routes/sse-router";
 import { Request, Response } from "express";
+import prisma from "./db/prisma";
 
 export const gracefulShutdown = (signal: "SIGINT" | "SIGTERM", server: any) => {
   console.log(`\n${signal} received. Starting graceful shutdown...`);
-  console.log(
-    "Closing HTTP server...",
-    connectedClients.size,
-    "clients to notify."
-  );
 
   // sagely notify clients
   connectedClients.forEach((res, clientId) => {
-    console.log(`Notifying client ${clientId} of shutdown.`);
     res.write(
-      `data: ${JSON.stringify({ type: "shutdown", payload: null })}\n\n`
+      `data: ${JSON.stringify({ type: "shutdown", payload: null })}\n\n`,
     );
     res.end();
   });
   connectedClients.clear(); // clear the map for memory management
 
   // shutdown actual server
-  server.close((err: any) => {
+  server.close(async (err: any) => {
+    // close database connections
+    await prisma.$disconnect();
+
     if (err) {
       console.error("Error during server shutdown:", err);
       process.exit(1);
     }
-    console.log("HTTP server closed.");
-
-    // drain other resources like DB connections
 
     console.log("Graceful shutdown complete. Exiting process.");
     process.exit(0);
   });
 
-  setTimeout(() => {
+  setTimeout(async () => {
     console.warn(
-      "Forcing shutdown after timeout. Some connections may not have closed gracefully."
+      "Forcing shutdown after timeout. Some connections may not have closed gracefully.",
     );
+    await prisma.$disconnect();
     process.exit(1);
   }, 10 * 1000);
 };
